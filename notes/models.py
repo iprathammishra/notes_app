57a05eca46e6221311ef11a601b252551b16b410
Nadia HYLARY <nhylary12@gmail.com>
2023-08-09 23:44:24 +0100

    added authentication and django rest framework

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200, )
+    content = models.TextField()
+    created = models.DateTimeField(auto_created=timezone.now(), editable=False)
+    updated = models.DateTimeField()
+    user = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        """On save, update timestamps"""
+        if not self.id:
+            self.created = timezone.now()
+        self.updated = timezone.now()
+        return super(Note, self).save(*args, **kwargs)
